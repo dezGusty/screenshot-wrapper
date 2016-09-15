3a00cb44910971d50bffea5e7d392a2ad2a168a9
SoloJr <mircea.solo95@yahoo.com>
2016-09-15 11:00:05 +0300

    SaveFileDialog Commit
    - added the option to select the name and path of the screenshot's name.

--- ScreenshotWrapper/ScreenshotWrapperWPF/ServiceLayer/ScreenshotWrapperOperations.cs
+++ ScreenshotWrapper/ScreenshotWrapperWPF/ServiceLayer/ScreenshotWrapperOperations.cs
@@ -10,6 +10,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using ScreenshotWrapperWPF.BusinessLogicLayer;
+using Microsoft.Win32;
+using ScreenshotWrapperWPF.Views;
+using System.IO;
+using System.Threading;
 
 namespace ScreenshotWrapperWPF.ServiceLayer
 {
@@ -18,7 +22,7 @@ namespace ScreenshotWrapperWPF.ServiceLayer
         #region dll imports
 
         [DllImport("Debug//ScreenshotWrapperDLL.dll", EntryPoint = "getAllDesktopsScreenshot", CharSet = CharSet.Unicode
-            ,CallingConvention = CallingConvention.Cdecl, ThrowOnUnmappableChar = true)]
+            , CallingConvention = CallingConvention.Cdecl, ThrowOnUnmappableChar = true)]
         public static extern void getAllDesktopsScreenshot(string filename);
 
         #endregion
@@ -58,18 +62,35 @@ namespace ScreenshotWrapperWPF.ServiceLayer
 
         public void TakeScreenshot(object param)
         {
-            TaskbarIcon tb = (TaskbarIcon)Application.Current.FindResource("NotifyIcon");
             try
             {
+                TaskbarIcon tb = (TaskbarIcon)Application.Current.FindResource("NotifyIcon");
                 string path = string.Empty;
-                if (this.config.IsUsingDateFormat == true)
+                if (this.config.IsSavedDirectly == true)
                 {
-                    path = this.helper.MakeDateFilename();
+                    if (this.config.IsUsingDateFormat == true)
+                    {
+                        path = this.helper.MakeDateFilename();
+                    }
+                    else
+                    {
+                        path = this.helper.MakeCountFilename();
+                        this.config.NumberOfScreenshots++;
+                        JSONOperations.Serialize(this.config);
+                    }
                 }
                 else
                 {
-                    path = this.helper.MakeCountFilename();
-                    this.config.NumberOfScreenshots++;
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.Filter = "PNG File (*.png)|*.png";
+                    sfd.InitialDirectory = Path.GetFullPath(this.config.OutputPath);
+                    sfd.DefaultExt = ".png";
+                    ToolWindow tw = new ToolWindow();
+                    tw.Show();
+                    sfd.ShowDialog(tw);
+                    path = sfd.FileName;
+                    tw.Close();
+                    Thread.Sleep(200);
                 }
 
                 getAllDesktopsScreenshot(path);
